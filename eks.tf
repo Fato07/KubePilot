d6c85a4d1ecd1e97de77d65406741fe8f26d73e8
Fathin Dosunmu <fathin@zenown.com>
2023-07-19 01:02:07 +0300

    eks node grouo update

--- eks.tf
+++ eks.tf
@@ -1,18 +1,20 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version = "~> 19.0"
+  version         = "~> 19.0"
   cluster_name    = "kubePilot-cluster"
   cluster_version = "1.27"
   subnet_ids      = ["subnet-0664059678e2e5d49", "subnet-0f040d4fc75678d58", "subnet-09166fd23123d4dcd"]
   vpc_id          = "vpc-0583b57a0e4ed943e"
-  
-  node_groups = {
-    eks_nodes = {
-      desired_capacity = 4
-      max_capacity     = 4
-      min_capacity     = 1
 
-      instance_type = "m5.large"
-    }
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = ["m6i.large", "m5.large", "m5n.large", "m5zn.large"]
+    min_size = 1
+    max_size = 4
+    desired_size = 4
+  }
+
+  eks_managed_node_groups = {
+    mygroup = {}
   }
 }
