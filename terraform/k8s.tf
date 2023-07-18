a407f483a22870d5618cbb35f8845251a946ef68
Fathin Dosunmu <fathin@zenown.com>
2023-07-19 02:23:46 +0300

    added github actions to push image to ECR repository

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -1,15 +1,3 @@
-resource "kubernetes_namespace" "kube-pilot-dev" {
-  metadata {
-    name = "kube-pilot-dev"
-  }
-}
-
-resource "kubernetes_namespace" "kube-pilot-prod" {
-  metadata {
-    name = "kube-pilot-prod"
-  }
-}
-
 resource "kubernetes_deployment" "kube-pilot-dev" {
   metadata {
     name = "kube-pilot-app"
@@ -18,7 +6,60 @@ resource "kubernetes_deployment" "kube-pilot-dev" {
 
   spec {
     replicas = 3
-    //... 
-    // the spec for your container will be placed here
+
+    selector {
+      match_labels = {
+        App = "kube-pilot-app"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          App = "kube-pilot-app"
+        }
+      }
+
+      spec {
+        container {
+          image = "<AWS_ACCOUNT_ID>.dkr.ecr.<AWS_REGION>.amazonaws.com/<ECR_REPOSITORY_NAME>:<IMAGE_TAG>"
+          name  = "kube-pilot-app"
+
+          port {
+            container_port = 8080
+          }
+
+          env {
+            name  = "ENVIRONMENT"
+            value = "dev"
+          }
+
+          env {
+            name  = "COMMIT_SHA"
+            value = "<COMMIT_SHA>"
+          }
+
+          readiness_probe {
+            http_get {
+              path = "/ready"
+              port = "8080"
+            }
+
+            initial_delay_seconds = 5
+            period_seconds        = 5
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/healthz"
+              port = "8080"
+            }
+
+            initial_delay_seconds = 5
+            period_seconds        = 5
+          }
+        }
+      }
+    }
   }
 }
